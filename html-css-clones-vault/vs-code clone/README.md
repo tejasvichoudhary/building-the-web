66fce2743eb52c5c2e80335b2a3cdae3e0878210
tejasvichoudhary <tejasvichoudhary1@gmail.com>
2026-08-03 18:53:39 +0530

    practice data-types Q

--- html-css-clones-vault/vs-code clone/README.md
+++ html-css-clones-vault/vs-code clone/README.md
@@ -3,7 +3,6 @@
  A front-end replica of the Visual Studio Code  homepage layout.
 
  ## Visual Preview
-<video src="images/preview.mov" width="100%" autoplay loop muted playsinline></video>
 
 ## Tech Stack Used
  * HTML5 (Semantic Structure)
---
+++ javascript-journey/data-types.js
@@ -0,0 +1,77 @@
+// Question-1 :   Input the distance in Kilometer and Convert into Meter and Centimeter.
+// What I solve
+let distanceInkilometer = 100; 
+
+ let meter = distanceInkilometer * 1000;
+ console.log(meter);
+
+ let centimeter = meter * 100
+ console.log(centimeter);
+
+ // What I learn 
+
+ const distanceInkilometer = 10;
+
+ const convertIntometer = (distanceInkilometer) =>{
+    return distanceInkilometer * 1000;
+ }
+ const convertIncentimeter = (distanceInkilometer) =>{
+    return distanceInkilometer * 100000;
+ }
+ console.log(`Convert into meter: ${convertIntometer(ditanceInkilometer)}`);
+ console.log(`Convert into centimeter: ${convertIncentimeter(distanceInkilometer)}`)
+
+// Question-2:    WAP to input radius of a circle and log the area of circle.
+ 
+// Good for one radius
+
+const radiusOFcircle = 5;
+const area = Math.PI * radiusOFcircle * radiusOFcircle
+console.log(area)
+
+// if there is multiple radius to find
+
+const radiusOFcircle = 10;
+
+const calculateCircleArea = (radiusOFcircle) => {
+   return  Math.PI * radiusOFcircle ** 2 
+}
+console.log(`Area of Circle: ${calculateCircleArea(radiusOFcircle)}`)
+
+// Question-3:    WAP to input two numbers and perform arithmetic operations on those numbers.
+
+const firstnumber = 30;
+const secondnumber = 20;
+
+const  multiply = (firstnumber,secondnumber ) =>{
+   return firstnumber * secondnumber 
+}
+const add = (firstnumber,secondnumber ) =>{
+   return firstnumber + secondnumber 
+}
+const subtract= (firstnumber,secondnumber ) =>{
+   return firstnumber - secondnumber 
+} 
+const divide = (firstnumber,secondnumber ) =>{
+   return firstnumber / secondnumber  
+}
+console.log(`Multiplication: ${multiply(firstnumber,secondnumber )}`)
+console.log(`Addition: ${add (firstnumber,secondnumber )}`)
+console.log(`Subtraction: : ${subtract(firstnumber,secondnumber )}`)
+console.log(`Division: ${divide(firstnumber,secondnumber )}`)
+
+// Question-4:    WAP to calculate total marks in two subject and then calculate percentage.
+
+const firstSubjectMarks = 80;
+const secondSubjectMarks = 90;
+
+const totalMarks = (firstSubjectMarks, secondSubjectMarks) => {
+    return firstSubjectMarks + secondSubjectMarks;
+}
+
+const percentage = (firstSubjectMarks, secondSubjectMarks) => {
+    return (totalMarks(firstSubjectMarks, secondSubjectMarks) / 200) * 100;
+}
+
+console.log(`Total Marks: ${totalMarks(firstSubjectMarks, secondSubjectMarks)}`);
+console.log(`Percentage: ${percentage(firstSubjectMarks, secondSubjectMarks)}`);
