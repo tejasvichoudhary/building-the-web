2f2ade9edcafb84d6dec67bb2c45371821bbae52
tejasvichoudhary <tejasvichoudhary1@gmail.com>
2026-08-09 22:37:08 +0530

    Q 2

--- javascript-journey/Operator.js
+++ javascript-journey/Operator.js
@@ -39,3 +39,39 @@ console.log(`${num1 / num2}`);
 }
 // Problem 2: Write a JavaScript program to demonstrate the use of assignment operators on variables.
 
+let operator = 20;
+ operator = operator + 5;
+ console.log("operator");
+
+ let num = 10;
+ let num2 = 20;
+
+ num = num2;
+ console.log(num); // it will be 20 it will overwrite 10 in num.
+
+ // Problem 3: Write a JavaScript program to compare two numbers and log whether they are equal, greater, or lesser.
+
+ let a = parseInt(prompt("Enter number first:"));
+ let b = parseInt(prompt("Enter number second:"));
+ if(a === b){
+    console.log("Equal")
+ } else if (a > b) {
+     console.log(a, " is Greater:" , b )
+ } else if ( a < b) {
+    console.log( b,"is Greater:")
+ }
+
+//  let a = parseInt(prompt("Enter number first:"));
+//  let b = parseInt(prompt("Enter number second:"));
+
+if (a > b){
+    console.log(a, "is Greater than", b);
+} else if ( a < b) {
+   console.log(a, "is smaller than", b);
+} else {
+    console.log(a, "is Equal to", b);
+}
+
+// Problem 4: Write a JavaScript program to demonstrate logical operators in a conditional statement.
+
+
