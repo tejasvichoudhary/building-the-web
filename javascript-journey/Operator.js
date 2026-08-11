763ec4cfd7c39b58454ee4c248ed737631c885b9
tejasvichoudhary <tejasvichoudhary1@gmail.com>
2026-08-11 23:10:29 +0530

    Q

--- javascript-journey/Operator.js
+++ javascript-journey/Operator.js
@@ -126,3 +126,33 @@ console.log(b >>> 2);
 
 // Problem 6: Write a JavaScript program to use the ternary operator to determine the larger of two numbers.
 
+let ter1 = 29
+let ter2 = 40 
+
+ter2 > ter1 ? "First num" : "Second num";
+
+console.log(ter1 > ter2 ? ter1 : ter2);
+
+// Problem 7: Write a JavaScript program to demonstrate type operators on different data types.
+
+console.log(typeof 10);
+
+console.log(typeof "hello");
+
+console.log(typeof true);
+
+console.log(typeof null);
+
+console.log(typeof undefined);
+
+console.log(typeof [1, 2, 3]);
+
+console.log(typeof { name: "Aman" });
+
+console.log(typeof function () { });
+
+//  new
+
+console.log(typeof new Date());
+
+console.log(typeof new Error());
