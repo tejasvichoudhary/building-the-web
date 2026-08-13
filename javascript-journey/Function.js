6e84da891bdcb5b00995a2347d85b4bd110656e7
tejasvichoudhary <tejasvichoudhary1@gmail.com>
2026-08-13 23:57:46 +0530

    q

---
+++ javascript-journey/Function.js
@@ -0,0 +1,9 @@
+// Question-1:  Write a function that takes a name as an argument and logs a greeting message to the console, like "Hello, [Name]!".
+
+function first (name){
+    console.log(`hello, ${name}`);
+}
+frist("tejasvi");
+
+// Question-2:  Write a function that takes two parameters, adds them together, and returns the result.
+
