21d61f7fb3f390e69fbd5410d3308b737f841847
tejasvichoudhary <tejasvichoudhary1@gmail.com>
2026-08-15 22:06:33 +0530

    q

--- javascript-journey/Function.js
+++ javascript-journey/Function.js
@@ -11,3 +11,20 @@ function parameters ( a ,b ){
     return(` two parameters${a + b}`)
 }
 console.log(parameters(20,30));
+
+// Question-3:  Write a function that takes the current hour as a parameter and logs a different greeting message based on whether it's morning, afternoon, or evening.
+
+const greeting = (time) => {
+
+    if (time >= 0 && time < 12) console.log("Good Morning");
+
+    else if (time >= 12 && time < 16) console.log("Good Afternoon");
+
+    else if (time >= 16 && time < 20) console.log("Good Evening");
+
+    else if (time >= 20 && time <= 24) console.log("Good Night");
+
+    else console.log("Invalid Time");
+}
+
+greeting(10);
