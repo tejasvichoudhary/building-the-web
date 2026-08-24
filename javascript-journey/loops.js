1c100ae4782dfe9117d347a90bf766c002fa4186
tejasvichoudhary <tejasvichoudhary1@gmail.com>
2026-08-24 19:39:49 +0530

    q

--- javascript-journey/loops.js
+++ javascript-journey/loops.js
@@ -264,3 +264,115 @@ for (let index = 1; index <= 5; index++) {
   console.log(star);
     
 }
+
+for (let i = 1; i <= 5; i++) {
+    let str = "";
+    for (let j = 1; j <= i; j++) {
+        str += "*";
+    }
+    console.log(str);
+}
+
+// Question-2: 
+
+// WAP to print this 
+
+//    *
+//   ***
+//  *****
+// *******
+
+for (let i = 1; i <= 7; i += 2) {
+
+    let star = "";
+
+    for (let j = 1; j <= (7 - i) / 2; j++) {
+        star += " ";
+    }
+    
+    for (let j = 1; j <= i; j++) {
+        star += "*";
+    }
+
+    console.log(star);
+}
+
+for (let i = 1; i <= 4; i++) {
+    let str = "";
+
+    for (let j = 1; j <= 7; j++) {
+        if (j >= 5 - i && j <= 3 + i) {
+            str += "*";
+        } 
+        else {
+            str += " ";
+        }
+    }
+
+    console.log(str);
+
+} 
+
+// Question-3:
+
+// WAP to print this
+
+// *****
+// *   *
+// *   *
+// *****
+
+for(let i = 1; i <= 4; i++){
+    let str = "";
+
+    for(let j = 1; j <= 4 ; j++) {
+        if (i == 1 || j == 1 || i == 4 || j == 4 ){
+            str += "*"
+        } else {
+             str += " ";
+        }
+    }
+        console.log(str)
+}
+
+// Question-4:
+
+// WAP to print this
+
+// 1
+// 12
+// 123
+// 1234
+// 12345
+
+for (let i = 1; i <= 5; i++){
+    let str = "";
+    for (let j = 1; j <= i; j++){
+        str += j
+    }
+    console.log(str)
+}
+
+// Question-5
+
+// WAP to print this
+
+//     A
+//    BBB
+//   CCCCC
+//  DDDDDDD
+// EEEEEEEEE
+
+// what i learn
+
+for(let i=1; i<=5; i++){
+    let str = "";
+    for(let j=1; j<=9; j++){
+        if(j>=6-i && j<=4+i){
+            str += String.fromCharCode(64+i);
+        }else{
+            str += " ";
+        }
+    }
+    console.log(str);
+}
