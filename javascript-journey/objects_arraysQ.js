b1c259ab4a33baf73ca6ff2c84b354dde3fd4f52
tejasvichoudhary <tejasvichoudhary1@gmail.com>
2026-09-16 23:07:01 +0530

    1 Q

--- javascript-journey/objects_arraysQ.js
+++ javascript-journey/objects_arraysQ.js
@@ -26,4 +26,30 @@ for (let index = 0; index < keys.length; index++) {
     result[key] = total;
 }
 
-console.log(result);
+// console.log(result);
+
+// 2. Count word occurrences in array
+
+let countword = ["apple", "banana", "apple", "orange", "banana", "apple"]
+
+// { apple: 3, banana: 2, orange: 1 }
+
+ 
+let obj = {}
+
+for (let index = 0; index < countword.length; index++) {
+
+    let currentWord = countword[index]
+
+    if (currentWord in obj) {
+
+        obj[currentWord]++
+
+    } else {
+
+        obj[currentWord] = 1
+
+    }
+}
+
+console.log(obj)
