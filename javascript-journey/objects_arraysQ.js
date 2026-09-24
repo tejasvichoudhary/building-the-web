7f3686dfc4811eae90e6f1cee9c896df08b0af68
tejasvichoudhary <tejasvichoudhary1@gmail.com>
2026-09-24 23:48:22 +0530

    Q

--- javascript-journey/objects_arraysQ.js
+++ javascript-journey/objects_arraysQ.js
@@ -239,6 +239,33 @@ for (let index = 0; index < highest.length; index++) {
 
 console.log(highAverageStudent);
 
+// Q12. Sort object entries by values (ascending)
+
+let asc = {
+    a: 3,
+    b: 1,
+    c: 2
+};
+
+let arr = Object.entries(asc);
+
+for (let i = 0; i < arr.length; i++) {
+
+    for (let j = 0; j < arr.length - 1; j++) {
+
+        if (arr[j][1] > arr[j + 1][1]) {
+
+            let temp = arr[j];
+
+            arr[j] = arr[j + 1];
+
+            arr[j + 1] = temp;
+        }
+    }
+}
+
+console.log(arr);
+
 
 
 
